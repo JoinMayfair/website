f9e0e4058dfad6039bbbd545795f30f787f11ff8
John Crickett <3667898+JohnCrickett@users.noreply.github.com>
2025-04-20 15:38:53 +0100

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({
-  integrations: [tailwind()]
-});
+import cloudflare from "@astrojs/cloudflare";
+
+ export default defineConfig({
+   integrations: [tailwind(), sitemap()],
+   adapter: cloudflare({
+     platformProxy: {
+       enabled: true,
+     },
+   }),
+ });
